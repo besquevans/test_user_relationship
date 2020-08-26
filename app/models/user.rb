7217be719bc69498f7ff27ce952b3593f3020801
Gavin Liu <du20783@gmail.com>
2020-08-27 01:37:45 +0800

    add follow relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        has_many :followingships, foreign_key: :following_id, class_name: "Follow"
+        has_many :followers, through: :followingships
+
+        has_many :followerships, foreign_key: :follower_id, class_name: "Follow"
+        has_many :followings, through: :followerships
 end
