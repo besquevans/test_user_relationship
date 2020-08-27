822641cb4b08ca08a9742f90af336ab1bc887c1e
Gavin Liu <du20783@gmail.com>
2020-08-27 09:13:30 +0800

    add follow self validate

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,14 @@
 class Follow < ApplicationRecord
+  validate :do_not_follow_self
   belongs_to :follower, foreign_key: "follower_id", class_name: "User"
   belongs_to :following, foreign_key: "following_id", class_name: "User"
+
+
+  private
+
+  def do_not_follow_self
+    if self.follower_id == self.following_id
+      errors.add(:follower, "can't follow self")
+    end
+  end
 end
