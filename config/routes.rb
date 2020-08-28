5411feff19795da609977a2ce1f5131498ba4f8f
Gavin Liu <du20783@gmail.com>
2020-08-28 16:17:49 +0800

    add follow action

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   root 'pages#index'
   resources :pages, only: [:index]
   devise_for :users
+  resources :users, only: [:index, :show] do 
+    resource :follows, only: [:create, :destroy]
+    
+    member do
+      get :fans 
+      get :followings
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
