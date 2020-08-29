e05bfece6ef59029ab39d6c5646fc54f4421e9a3
Gavin Liu <du20783@gmail.com>
2020-08-29 22:29:29 +0800

    add avatar for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-        has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
-        has_many :fans, through: :followingships, source: :fan
+  has_one_attached :avatar    #大頭照
+  has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
+  has_many :fans, through: :followingships, source: :fan
 
-        has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
-        has_many :followings, through: :fanships, source: :following
+  has_many :fanships, foreign_key: :fan_id, class_name: "Follow", dependent: :destroy
+  has_many :followings, through: :fanships, source: :following
 end
