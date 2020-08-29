bb4aedcfa34d683194a9c8f88b61a0e021621f35
Gavin Liu <du20783@gmail.com>
2020-08-29 22:22:44 +0800

    add counter and nick_name for users

---
+++ db/migrate/20200829141531_add_nick_name_to_user.rb
@@ -0,0 +1,9 @@
+class AddNickNameToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :nick_name, :string   
+    add_column :users, :posts_count, :integer, default: 0
+    add_column :users, :fans_count, :integer, default: 0
+    add_column :users, :followings_count, :integer, default: 0
+    add_column :users, :collections_count, :integer, default: 0
+  end
+end
