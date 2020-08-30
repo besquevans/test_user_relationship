7281258e25fb2b03684226e4f2a81e3a3f47fef7
Gavin Liu <du20783@gmail.com>
2020-08-30 17:37:49 +0800

    add avatar to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_one_attached :avatar    #大頭照
-  has_many :posts 
+  # has_one_attached :avatar    #大頭照
+  mount_uploader :avatar, AvatarUploader
+
+  has_many :posts
   has_many :comments
   has_many :followingships, foreign_key: :following_id, class_name: "Follow", dependent: :destroy
   has_many :fans, through: :followingships, source: :fan
